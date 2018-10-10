d430a48b34f1260f0f34b4fcef5524d3963815b5
Adrien <verblodung@gmail.com>
2018-10-09 23:43:11 -0400

    add js to newtab

---
+++ js/newtab.js
@@ -0,0 +1,17 @@
+chrome.storage.local.get(['opens', 'lastOpen', 'color', 'emoji'], function(data) {
+  document.body.style['background-color'] = data.color || '#212127'
+  document.getElementsByClassName('cat')[0].innerHTML = data.emoji || '🐈'
+  if (!Number.isInteger(data.opens) || data.lastOpen !== new Date().getDay()) {
+    updateOpens(0)
+  } else {
+    updateOpens(data.opens + 1)
+  }
+})
+
+const updateOpens = newTotal => {
+  if (newTotal < 324) document.body.style['font-size'] = 20 + newTotal + 'px'
+  document.getElementsByClassName('cat')[0].title = newTotal + ' new tabs today'
+  chrome.storage.local.set({ opens: newTotal, lastOpen: new Date().getDay() }, function() {
+    console.log('opens is set to ' + newTotal)
+  })
+}
